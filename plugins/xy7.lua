130b1586b0bd38141eb37481642e20f66dd79f9e
sam1382 <samiiiiiiiiiiiiiiiiiiiiiiii24@gmail.com>
2016-06-18 02:15:57 -0700

    Create xy7.lua

---
+++ plugins/xy7.lua
@@ -0,0 +1,16 @@
+do
+local function run(msg, matches)
+local bot_id = 110001039
+local fbot1 = 187419654
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
+    end
+end
+
+return {
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
